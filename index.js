a2c7536d07e8c5cf160cafc040a9de965b0aa9a8
Simon Garber <Simon.garber@gmail.com>
2019-07-13 15:38:18 -0400

    completed and passed all tests

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+
+const sumItems = function(array) {
+  let total = 0;
+  for(let i = 0; i < array.length; i++) {
+    if(Array.isArray(array[i])) {
+      total+=sumItems(array[i])
+      } else {
+      total += array[i]
+    }
+    
+  }
+  return total;
 }
 
+  
+
+   
+    
+    
+
+  
+
+
+  
+  
+
+
+
+// sumItems([1,2,3,4,5])
 module.exports = sumItems;
